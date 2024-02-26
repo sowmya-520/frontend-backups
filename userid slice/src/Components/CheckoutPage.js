7728e43448f08dba41ab91881d592d3281a204d0
Shiva Sowmya <67231807+sowmya-520@users.noreply.github.com>
2024-02-26 15:42:47 +0530

    customerid updated

---
+++ userid slice/src/Components/CheckoutPage.js
@@ -0,0 +1,10 @@
+import React from "react";
+// import { UseDispatch, useSelector } from "react-redux";
+const CheckoutPage = () => {
+  // const cartItems = useSelector((state) => state.cart.cartItems);
+  return (
+   
+      <div>checkoutpage</div>
+  );
+}
+export default CheckoutPage;
---
+++ userid slice/src/Components/DashboardPage.js
@@ -0,0 +1,13 @@
+import CategoryHomePage from './CategoryHomePage';
+import DashBoardNav from './DashBoardNav';
+
+const Dashboard = () => {
+  return (
+    <div>
+      <DashBoardNav/>
+      <CategoryHomePage/>
+    </div>
+  );
+};
+
+export default Dashboard;
